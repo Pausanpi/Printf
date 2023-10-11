81ccdf12fe30b03fd724b9aa609f1ce2cbf82d7d
Paula Sánchez <123211878+Pausanpi@users.noreply.github.com>
2023-10-11 17:03:04 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: pausanch <pausanch@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/09/27 10:41:33 by pausanch          #+#    #+#              #
-#    Updated: 2023/09/27 10:52:03 by pausanch         ###   ########.fr        #
+#    Updated: 2023/10/11 11:27:59 by pausanch         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -31,13 +31,17 @@ LIB = ar rcs
 all: $(NAME)
 
 $(NAME): $(OBJ)
-	$(LIB) $(NAME) $(OBJ)
+	@$(LIB) $(NAME) $(OBJ)
+
+%.o:	%.c
+	@printf "\e[38;5;211mCompilinge \e[38;5;122m$<\e[0m\n"
+	@gcc $(CFLAGS) -c $<
 
 clean:
-	rm -f $(OBJ)
+	@rm -f $(OBJ)
 
-fclean:
-	rm -f $(NAME)
+fclean: clean
+	@rm -f $(NAME)
 
 re: fclean all
 
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pausanch <pausanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 10:43:44 by pausanch          #+#    #+#             */
-/*   Updated: 2023/09/27 12:37:32 by pausanch         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:43:50 by pausanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,21 @@ static int	ft_conver(char const *content, int x, va_list *args)
 	count = 0;
 	if (content[x] == 'c')
 		count += ft_putchar(va_arg(*args, int));
-	if (content[x] == 's')
+	else if (content[x] == 's')
 		count += ft_string(va_arg(*args, char *));
-	if (content[x] == 'p')
+	else if (content[x] == 'p')
 		count += ft_pointer(va_arg(*args, unsigned long long));
-	if (content[x] == 'd')
+	else if (content[x] == 'd')
 		count += ft_decimal(va_arg(*args, int));
-	if (content[x] == 'i')
+	else if (content[x] == 'i')
 		count += ft_decimal(va_arg(*args, int));
-	if (content[x] == 'u')
+	else if (content[x] == 'u')
 		count += ft_unsigned(va_arg(*args, unsigned int));
-	if (content[x] == 'x')
+	else if (content[x] == 'x')
 		count += ft_hexa(va_arg(*args, unsigned int), 'x');
-	if (content[x] == 'X')
+	else if (content[x] == 'X')
 		count += ft_hexa(va_arg(*args, unsigned int), 'X');
-	if (content[x] == '%')
+	else if (content[x] == '%')
 		count += ft_putchar('%');
 	return (count);
 }
@@ -64,11 +64,16 @@ int	ft_printf(char const *content, ...)
 
 /* int	main(void)
 {
-	int	x;
+	char *s = "hey!";
+	void *p = NULL;
+	int	d = -3;
+	unsigned int x = 0x40;
+	int i = -27;
+	unsigned int u = 690;
+	char c = 'g';
 
 	x = 922;
-	printf("Hola %x\n", x);
-	ft_printf("Hola %x\n", x);
+	printf("%i\n",printf("%s%p%d%x%i%u%c%%\n", s, p, d, x, i, u, c));
+	printf("%i\n",ft_printf("%s%p%d%x%i%u%c%%\n", s, p, d, x, i, u, c));
 	return (0);
-}
- */
+} */
